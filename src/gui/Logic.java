fcd4144a8bb2cb2a8196b30fc757fb0f23ce0894
Hiatel <minhtra@outlook.com>
2018-12-30 15:00:10 -0500

    added button bar and some logic scraps

--- src/gui/Logic.java
+++ src/gui/Logic.java
@@ -1,15 +1,39 @@
 package gui;
+//Import hell 0, java
 
+//Import hell 1, javafx
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
-public class Logic {
-    public Label helloWorld;
+//Import hell 2, java io
+
+
+public class Logic { //-----button logic for button functions
+    public Label Feedback;
+
+    //-----File logic for creating the output string file.
+    /*    File dir = new File("io/test");
+        dir.mkdirs();
+    File tmp = new File(dir, "tmp.txt");
+        tmp.createNewFile();*/
+
+    int one = 1;
+    String Outstring = "";
 
 
     public void sayTESTCallResponse(ActionEvent actionEvent) {
-        helloWorld.setText("Response");
+        Feedback.setText("Response");
+        one += 1;
+        Outstring += "wungus";
     }
 
 
+    public void modifylocation(ActionEvent actionEvent) {
+        /*Feedback.setText(String.valueOf(one));*/
+        Feedback.setText(Outstring);
+    }
+
+    public void resetallfields(ActionEvent actionEvent) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,3 +1,4 @@
+package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,16 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
+    @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("./gui/interface.fxml"));
-        primaryStage.setTitle("AAV Photo UI");
-        primaryStage.setScene(new Scene(root, 1200, 800));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
 
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
